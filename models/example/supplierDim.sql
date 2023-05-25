582ad08a37a02945c9c2edf6d9e0133df31a0705
jperkins@eidebailly.com <jperkins@eidebailly.com>
2023-05-25 16:51:33 +0000

    iteration 1

---
+++ models/example/supplierDim.sql
@@ -0,0 +1,7 @@
+{{ config(database = "snowflake_sample_data") }}
+SELECT s.s_suppkey as supplier_key
+,s.s_name as name
+,s.s_comment as comment
+,s.s_phone as phone
+
+FROM {{ source('TPCH_SF1000', 'Supplier')}} AS s
